0dd6b8d352bee4901a4de2f8b22291cdf8f16ed5
Vincent.Ei-HTL <86598196+Vinc040@users.noreply.github.com>
2024-02-12 00:14:17 +0100

    added logout and login

--- Auth/AadAuth/src/app/app.component.ts
+++ Auth/AadAuth/src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MsalService } from '@azure/msal-angular';
 import { AuthenticationResult } from '@azure/msal-browser';
-
+import * as MicrosoftGraph from "@microsoft/microsoft-graph-types";
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   loggedIn: boolean = false;
+  profile?: MicrosoftGraph.User;
   title = 'aad-auth';
 
   constructor(private authService: MsalService, private client: HttpClient) {
@@ -36,4 +37,16 @@ export class AppComponent {
         this.checkAccount();
       });
   }
+
+  logout() {
+    this.authService.logout();
+    this.loggedIn = false;
+  }
+
+  getProfile() {
+    this.client
+      .get<MicrosoftGraph.User>("https://graph.microsoft.com/v1.0/me")
+      .subscribe((profile) => (this.profile = profile));
+  }
+
 }
