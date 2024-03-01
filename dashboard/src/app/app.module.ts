db3b550b55cf5bdf02cd03381756e54f52bc2b60
Marcus Rabeder <rabeder.marcus@gmail.com>
2024-03-01 09:33:52 +0100

    filter fixed

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatSelectModule } from '@angular/material/select';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentOverviewComponent } from './student-overview/student-overview.component';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { MatOptionModule } from '@angular/material/core';
+
+
 
 @NgModule({
   declarations: [
@@ -11,8 +19,13 @@ import { StudentOverviewComponent } from './student-overview/student-overview.co
     StudentOverviewComponent
   ],
   imports: [
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
