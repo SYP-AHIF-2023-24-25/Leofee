5e9ac7b30c2a49d98f966935fe4a5238fde35019
Marcus Rabeder <rabeder.marcus@gmail.com>
2024-03-05 08:28:19 +0100

    backend added

---
+++ backend/Core/ImportData/Model/Bon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportData.Model
+{
+    public class Bon
+    {
+        private int studentID = 0;
+        private double value  = 0;
+        private DateTime start;
+        private DateTime end;
+        public Bon(int studentId,double value) {
+            this.studentID = studentId;
+            this.value = value;
+        }
+    }
+}
---
+++ backend/Core/ImportData/Model/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections;
+namespace ImportData.Model
+{
+    public class Student
+    {
+        private int id;
+        public string firstname { get; set; } = string.Empty;
+        public string lastname { get; set; } = string.Empty;
+        private string password { get; set; } = string.Empty;
+        public string email { get; set; } = string.Empty;
+        public string schoolClass { get; set; } = string.Empty;
+        private List<Bon> bonList = new List<Bon>();
+
+        public Student(string firstname, string lastname,string password,string email, string schoolClass)
+        {
+            string idString = firstname + lastname + password;
+            //wird noch geändert (UUID)
+            this.id = int.Parse(GenerateSHA256Hash(idString));
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.password = password;
+            this.email = email;
+            this.schoolClass = schoolClass;
+
+        }
+        static string GenerateSHA256Hash(string input)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                StringBuilder builder = new StringBuilder();
+
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2")); 
+                }
+
+                return builder.ToString();
+            }
+        }
+
+    }
+    
+}
