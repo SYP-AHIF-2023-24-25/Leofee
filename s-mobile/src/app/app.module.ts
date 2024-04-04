1da83a02e795ebf79ce06508a186ec3b47c2ce9e
Michael Werner <michael.werner@24speed.at>
2024-04-04 10:54:58 +0200

    merge conflicts

--- s-mobile/src/app/app.module.ts
+++ s-mobile/src/app/app.module.ts
@@ -6,19 +6,47 @@ import { AppComponent } from './app.component';
 import { QrCodeGeneratorComponent } from './qr-code-generator/qr-code-generator.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { GiftCardComponent } from './gift-card/gift-card.component';
+import { MsalModule } from '@azure/msal-angular';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MSAL_INSTANCE, MSAL_INTERCEPTOR_CONFIG, MsalInterceptor, MsalInterceptorConfiguration, MsalService } from '@azure/msal-angular';
+import { IPublicClientApplication, PublicClientApplication } from '@azure/msal-browser';
+import { environment } from './environments/environment';
+import { MSALInstanceFactory, MSALInterceptorConfigFactory } from './auth/msal';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     QrCodeGeneratorComponent,
-    LoginComponent
+    LoginComponent,
+    GiftCardComponent
+
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MsalModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+    MsalService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
