b4f8edd9fce8f4d5549a30770a3e1b7bc61dff2f
Michael Werner <michael.werner@24speed.at>
2024-05-15 08:48:58 +0200

    resolve merge conflicts

--- backend/Core/Api/Program.cs
+++ backend/Core/Api/Program.cs
@@ -1,14 +1,14 @@
 
 using Core;
 using Microsoft.AspNetCore.Http.HttpResults;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
-string bonsPath = @"./bons.txt";
-string personsPath = @"./personal.txt";
 
 
-var students = ImportData.DataController.importStudents(personsPath);
-var bons = ImportData.DataController.importBons(bonsPath);
+
+var students = ImportData.DataController.importStudents();
+var bons = ImportData.DataController.importBons();
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,7 +41,7 @@ app.UseCors(policy => policy
 
 app.MapGet("/students", () =>
 {
-    //var ids = students.Select(student => student.id).ToList();
+    
     return Results.Ok(students);
 });
 app.MapGet("/students/{id}", (string id) =>
@@ -60,21 +60,63 @@ app.MapGet("/student/{id}/balance", (string id) =>
     var balanceForStudent = ImportData.Controller.getBalanceFromAllBons(bonsForStudent);
     return Results.Ok(balanceForStudent);
 });
-app.MapGet("student/{studentString}/getId", (string studentString) =>
-{
-    string studentId = Student.GenerateSHA256Hash(studentString);
-    return studentId;
-});
 app.MapPost("/student/{id}/pay/{value}", (string id, double value) =>
 {
     var bonsForStudent = ImportData.Controller.getValidBonsForStudent(id,bons,students,DateTime.Now);
-    ImportData.Controller.Pay(bonsForStudent, value);
+    var finishedResult = ImportData.Controller.Pay(bonsForStudent, value);
+    if (finishedResult == false)
+    {
+        return Results.BadRequest();
+
+    }
+    bons = ImportData.DataController.importBons();
+    return Results.Ok();
+});
+app.MapPost("/student/{creationString}", (string creationString) =>
+{
+    var result = ImportData.Controller.addStudent(creationString);
+    if (result == false)
+    {
+        return Results.Problem();   
+
+    }
+    students = ImportData.DataController.importStudents();
+    return Results.Ok();
 });
 
 app.MapDelete("/student/{id}", (string id) =>
 {
-    
+    var result = ImportData.Controller.deleteStudent(int.Parse(id));
+    if (result == false)
+    {
+        return Results.Problem();
+
+    }
+    students = ImportData.DataController.importStudents();
+    return Results.Ok();
+});
+app.MapPost("/bon/{creationString}", (string creationString) =>
+{
+    var result = ImportData.Controller.addBon(creationString);
+    if (result == false)
+    {
+        return Results.Problem();
+
+    }
+    bons = ImportData.DataController.importBons();
+    return Results.Ok();
 });
 
+app.MapDelete("/bon/{id}", (string id) =>
+{
+    var result = ImportData.Controller.deleteBon(int.Parse(id));
+    if (result == false)
+    {
+        return Results.Problem();
+
+    }
+    bons = ImportData.DataController.importBons();
+    return Results.Ok();
+});
 
 app.Run();
