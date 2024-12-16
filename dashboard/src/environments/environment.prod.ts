eb891c048a95075af1d2dcb59b53f5d33cff6f61
Marcus-Rabeder <rabeder.marcus@gmail.com>
2024-12-16 10:01:04 +0100

    env wurde auf localhost geswitched

--- dashboard/src/environments/environment.prod.ts
+++ dashboard/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,   
-    //httpUrlLeofeeBackend: "http://localhost:5015/",  
-    httpUrlLeofeeBackend: "https://vm64.htl-leonding.ac.at/leofee-backend/", 
+    httpUrlLeofeeBackend: "http://localhost:5015/",  
+    //httpUrlLeofeeBackend: "https://vm64.htl-leonding.ac.at/leofee-backend/", 
     //httpUrlLeofeeBackend: "http://localhost:5015/",
   };
